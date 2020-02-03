bb0ee43479ddad5ac5c6b2d89e3a1d89159c53fb
Iliass JABALI <i.jabali@aui.ma>
2020-02-03 19:22:25 +0100

    added the Kevin's codes

---
+++ CSC 2302/Jan/Class codes/shakespeare.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h> 
+
+void main() {
+	FILE *file = fopen("poetry.txt", "rw");
+	int lines, i;
+	char line[100];
+	
+	fscanf(file, "%d", &lines);
+	
+	for(i = 0; i < lines; i++) {
+	//while(!feof(infile)) {
+		fgets(line, 80, file);
+		printf("Shakespeare wrote: %s", line);
+		printf("Give me a line from La Fontaine: ");
+		fgets(line, 80, stdin);
+		fprintf(file, "%s", line);
+		}
+		
+	
+	fclose(file);
+}
